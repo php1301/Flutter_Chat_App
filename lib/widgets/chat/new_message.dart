f05ed98b472c6097c782792ab502ee42a4e68e29
php1301 <luudanthanchet@gmail.com>
2020-10-03 21:24:38 +0700

    NewMessage added

---
+++ lib/widgets/chat/new_message.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class NewMessage extends StatefulWidget {
+  @override
+  _NewMessageState createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  var _enteredMessage = '';
+  void _sendMessage() async {
+    FocusScope.of(context).unfocus();
+    final user = await FirebaseAuth.instance.currentUser();
+    final userData =
+        await Firestore.instance.collection('users').document(user.uid).get();
+    Firestore.instance.collection('chats').add({
+      'text': _enteredMessage,
+      'createdAt': Timestamp.now(),
+      'userId': user.uid,
+      'username': userData['username'],
+      'userImage': userData['image_url'],
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 8),
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              textCapitalization: TextCapitalization.sentences,
+              enableSuggestions: true,
+              autocorrect: true,
+              decoration: InputDecoration(
+                labelText: 'Send a message...',
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _enteredMessage = value;
+                });
+              },
+            ),
+          ),
+          IconButton(
+            color: Theme.of(context).primaryColor,
+            icon: Icon(Icons.send),
+            onPressed: _enteredMessage.trim().isEmpty ? null : _sendMessage,
+          ),
+        ],
+      ),
+    );
+  }
+}
