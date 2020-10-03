ff5e003fcf42feba7251a4a526cd548b46b85575
php1301 <luudanthanchet@gmail.com>
2020-10-03 21:24:58 +0700

    UserImagePicker with image_picker package

---
+++ lib/widgets/pickers/user_image_picker.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  final void Function(File pickedImage) imagePickFn;
+  UserImagePicker(this.imagePickFn);
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+  void _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImageFile = await picker.getImage(source: ImageSource.camera, imageQuality: 50, maxWidth: 150, );
+    setState(() {
+      _pickedImage = File(pickedImageFile.path);
+    });
+    widget.imagePickFn(File(pickedImageFile.path));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage) : null,
+        ),
+        FlatButton.icon(
+          onPressed: _pickImage,
+          icon: Icon(Icons.image),
+          label: Text('Add Image'),
+          textColor: Theme.of(context).primaryColor,
+        ),
+      ],
+    );
+  }
+}
